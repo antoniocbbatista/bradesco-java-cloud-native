28731a3ca24e7c500f349a6b76afdaff0a53c2c6
antoniocbbatista <antoniocarlosdebritobatista@hotmail.com>
2025-02-18 20:28:32 -0300

    Initial commit

---
+++ src/main/java/com/dio/bradesco_java_cloud_native/BradescoJavaCloudNativeApplication.java
@@ -0,0 +1,13 @@
+package com.dio.bradesco_java_cloud_native;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BradescoJavaCloudNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BradescoJavaCloudNativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/bradesco_java_cloud_native/BradescoJavaCloudNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.bradesco_java_cloud_native;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BradescoJavaCloudNativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
